529d1c393010d42444a65bf3eb9dc615e6318c16
Ken Schnall <kenneth.schnall@gmail.com>
2016-01-25 13:20:05 -0500

    modify problem 7

--- 7.py
+++ 7.py
@@ -1,5 +1,9 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-def findPrimeNumbers(10001): # find the 10,001st prime number
-	array = []
+def findPrimeNumbers(number): # find the 10,001st prime number
+	array=[] # contains prime numbers
+	while len(array)<10001:
+		
+		
+findPrimeNumbers(10001)
