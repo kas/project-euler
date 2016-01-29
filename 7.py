7c5dee1a6e1c1d12979bfbbe39cdd5e409e0aa2e
Ken Schnall <kenneth.schnall@gmail.com>
2016-01-29 00:06:51 -0500

    finish problem 7

--- 7.py
+++ 7.py
@@ -2,8 +2,16 @@
 # What is the 10 001st prime number?
 
 def findPrimeNumbers(number): # find the 10,001st prime number
-	array=[] # contains prime numbers
+	array=[2,3,5,7,11,13] # contains first 6 prime numbers
+	y=15
 	while len(array)<10001:
-		
+		print len(array)
+		for x in reversed(range(1, y, 2)):
+			if(y%x==0 and x!=1): # y is not a prime number
+				break
+			elif(x==1): # y is a prime number
+				array.append(y)
+		y+=2 # go to next odd number
+	print array[-1]
 		
 findPrimeNumbers(10001)
