51fb399c6017b2c5b04c0f28468ae130837ead97
Ken Schnall <kenneth.schnall@gmail.com>
2016-03-24 14:50:25 -0400

    finish 063.py

---
+++ 063.py
@@ -0,0 +1,14 @@
+# The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+
+# How many n-digit positive integers exist which are also an nth power?
+
+from fractions import Fraction
+
+numbers = []
+
+for x in range(1,101):
+	for y in range(1,101):
+		if (len(str(x**y))==y):
+			numbers.append(x**y)
+
+print("length: ",len(numbers))
