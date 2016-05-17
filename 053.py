6cb9c91694d29de3c5b02dbed161c5c5be39ac8c
Ken Schnall <kenneth.schnall@gmail.com>
2016-05-16 21:51:23 -0400

    finish problem 053

---
+++ 053.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# There are exactly ten ways of selecting three from five, 12345:
+
+# 123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+# In combinatorics, we use the notation, 5C3 = 10.
+
+# In general,
+
+# nCr =	
+# n!
+# r!(n−r)!
+# ,where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
+# It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+
+# How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are greater than one-million?
+
+import math
+
+ONE_MILLION = 1000000
+
+values = []
+
+for n in range(1,101):
+	for r in range(0,n):
+		combination = (math.factorial(n))/((math.factorial(r))*(math.factorial(n-r)))
+		if (combination > ONE_MILLION):
+			values.append(combination)
+
+print(len(values))
